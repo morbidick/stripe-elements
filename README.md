42b7154c598d8b1a77847eb24b6454e211028678
morbidick <mail@morbi-happens.de>
2018-06-28 12:13:21 +0200

    modulizer run and cleanup

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
-# Polymer 2 Stripe Elements
+# Stripe Web Components
 
 [![Published on webcomponents.org](https://img.shields.io/badge/webcomponents.org-published-blue.svg)](https://www.webcomponents.org/element/morbidick/stripe-elements)
 [![Build Status](https://travis-ci.org/morbidick/stripe-elements.svg?branch=master)](https://travis-ci.org/morbidick/stripe-elements)
 
 Simple component talking directly with the [Stripe](https://stripe.com/) [token backend](https://stripe.com/docs/api#tokens). It doesnt use stripe.js so it doesnt have the same ShadowDOM restrictions. The form is build with paper-elements to fit the Material UI.
+
+## Installation
+
+```bash
+npm i @morbidick/stripe-elements
+```
 
 ## Components
 
@@ -13,8 +19,8 @@ Simple component talking directly with the [Stripe](https://stripe.com/) [token 
 ```
 <custom-element-demo>
   <template>
-    <script src="../webcomponentsjs/webcomponents-lite.js"></script>
-    <link rel="import" href="stripe-card.html">
+    <script src="../@webcomponents/webcomponentsjs/webcomponents-loader.js"></script>
+    <script type="module" src="stripe-card.js"></script>
     <next-code-block></next-code-block>
   </template>
 </custom-element-demo>
@@ -25,7 +31,6 @@ Simple component talking directly with the [Stripe](https://stripe.com/) [token 
 <stripe-card
   publishable-key="pk_your_key"
   token="{{ token }}"
-  mask-card
 ></stripe-card>
 ```
 
---
+++ stripe-card.js
@@ -0,0 +1,289 @@
+import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+import '@polymer/polymer/lib/elements/dom-if.js';
+import '@polymer/iron-form/iron-form.js';
+import '@polymer/paper-button/paper-button.js';
+import '@polymer/paper-input/paper-input.js';
+import '@polymer/paper-styles/default-theme.js';
+import '@polymer/paper-styles/typography.js';
+/**
+ * @customElement
+ * @polymer
+ * @extends {Polymer.Element}
+ */
+class StripeCard extends PolymerElement {
+  static get template() {
+    return html`
+    <style>
+      :host {
+        display: block;
+      }
+      .date-separator {
+        margin: 0.4em 0.3em 0;
+        font-size: 1.3em;
+      }
+      .horizontal {
+        display: flex;
+      }
+      .wrap {
+        flex-wrap: wrap;
+      }
+      .wrap > *, #error {
+        margin: 0 0.5em;
+      }
+      .small {
+        width: 2em;
+        flex-shrink: 0;
+      }
+      .medium {
+        flex-grow: 1;
+        width: 8em;
+      }
+      .large {
+        flex-grow: 2;
+        min-width: 20em;
+      }
+      #error {
+        min-height: 1em;
+        color: var(--paper-input-container-invalid-color, var(--error-color));
+        @apply --paper-font-caption;
+        @apply --paper-input-error;
+      }
+      /* hide input number selector */
+      paper-input[type="number"] {
+        --paper-input-container-input: {
+          -moz-appearance: textfield;
+          appearance: textfield;
+        };
+        --paper-input-container-input-webkit-spinner: {
+          -webkit-appearance: none;
+          appearance: none;
+          margin: 0;
+        };
+      }
+    </style>
+
+    <iron-form id="form">
+      <form>
+        <div class="horizontal wrap">
+          <paper-input id="cardNumber" class="large" type="tel" allowed-pattern="[0-9 ]" name="number" label="Card number" auto-validate="" required="" no-label-float="" on-value-changed="_maskCard"></paper-input>
+          <div class="horizontal">
+            <paper-input class="small" type="tel" name="exp_month" label="MM" min="1" max="12" maxlength="2" auto-validate="" required="" no-label-float=""></paper-input>
+            <div class="date-separator">/</div>
+            <paper-input class="small" type="tel" name="exp_year" label="YY" maxlength="2" auto-validate="" required="" no-label-float=""></paper-input>
+          </div>
+          <paper-input class="medium" type="tel" name="cvc" label="CVC" maxlength="4" auto-validate="" required="" no-label-float=""></paper-input>
+          <template is="dom-if" if="[[ !hideZip ]]">
+            <paper-input class="medium" type="text" name="address_zip" label="ZIP" no-label-float=""></paper-input>
+          </template>
+        </div>
+        <div id="error"></div>
+        <template is="dom-if" if="[[ !hideSubmit ]]">
+          <paper-button on-click="_submit">[[submitLabel]]</paper-button>
+        </template>
+      </form>
+    </iron-form>
+  `;}
+
+  static get is() { return 'stripe-card'; }
+
+  static get properties() {
+    return {
+      // Your stripe publishable key (https://stripe.com/docs/dashboard#api-keys)
+      publishableKey: {
+        type: String,
+      },
+      // Stripe token response (https://stripe.com/docs/api#token_object-id)
+      token: {
+        type: Object,
+        notify: true,
+      },
+      // Stripe api endpoint
+      _apiUrl: {
+        type: String,
+        value: 'https://api.stripe.com/v1/tokens',
+      },
+      // Stripe api headers Object
+      _apiHeaders: {
+        type: Object,
+        value: () => {
+          return new Headers({
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Accept": "application/json",
+            // Stripe api version (https://stripe.com/docs/api#versioning)
+            "Stripe-Version": "2017-12-14",
+          });
+        },
+      },
+      // Whether to show zip-code field
+      hideZip: {
+        type: Boolean,
+        value: false,
+        reflectToAttribute: true,
+      },
+      // Whether to display the submit button
+      hideSubmit: {
+        type: Boolean,
+        value: false,
+        reflectToAttribute: true,
+      },
+      maskChar: {
+        type: String,
+        value: ' ',
+      },
+      // Is true when the request is processing
+      loading: {
+        type: Boolean,
+        value: false,
+        notify: true,
+      },
+      submitLabel: {
+        type: String,
+        value: 'Submit',
+      },
+    };
+  }
+
+  // request token, returns a promise
+  async createToken() {
+    this.loading = true;
+    let form = this.$.form;
+
+    // some basic client side validation
+    if (!form.validate()) {
+      throw {
+        type: 'form_validation',
+      };
+    };
+
+    let payload = {
+      'key': this.publishableKey,
+      'referrer': document.location.href,
+      'card': form.serializeForm(),
+    };
+
+    let body;
+    try {
+      let response = await fetch(this._apiUrl, {
+        method: 'POST',
+        body: this._nestedQueryString(payload),
+        headers: this._apiHeaders,
+      });
+      body = await response.json();
+    } catch(error) {
+      this.loading = false;
+      throw {
+        type: 'api_connection_error',
+        message: 'Connection to Stripe api failed.',
+        detail: error,
+      };
+    }
+
+    this.loading = false;
+    this._clearErrors();
+
+    if (body.id) {
+      this.token = body;
+      return body;
+    } else if (body.error) {
+      this.displayError(body.error);
+      throw body.error;
+    } else {
+      throw {
+        type: 'api_error',
+        message: 'Unknown error',
+      };
+    }
+  }
+
+  reset() {
+    this._clearErrors();
+    this.$.form.reset();
+    this.token = null; // see https://github.com/Polymer/polymer/issues/2565
+  }
+
+  _maskCard(event) {
+    let input = event.composedPath()[0]._focusableElement;
+    let value = input.value;
+    let cursor = input.selectionStart;
+    let previousCharASpace = value ? String(value).charAt(cursor - 1) == this.maskChar : false;
+
+    // Bail if empty
+    if (!value) { return };
+
+    value = value
+      // remove all masking characters
+      .replace(new RegExp(this.maskChar, 'g'), '')
+      // split in parts of 4 digits
+      .match(/.{1,4}/g)
+      // join by mask char
+      .join(this.maskChar);
+
+    // If the character right before the selection is a newly inserted
+    // maskChar, we need to advance the selection to maintain the caret position.
+    if (!previousCharASpace && value.charAt(cursor - 1) == this.maskChar) {
+      cursor = cursor + 1;
+    }
+
+    input.value = value;
+    // set cursor
+    input.selectionStart, input.selectionEnd = cursor;
+  }
+
+  _submit() {
+    this.createToken().catch((error) => {/* suppress uncaught promise warnings */});
+  }
+
+  _clearErrors() {
+    this.$.error.innerText = "";
+  }
+
+  displayError(error) {
+    if (error.type == "card_error" && error.param) {
+      let element = this.$.form.querySelector(`[name="${error.param}"]`);
+
+      if (element) {
+        element.invalid = true;
+        if (error.param == "number") {
+          element.errorMessage = error.message;
+          return;
+        }
+      }
+    }
+    this.$.error.innerText = error.message;
+  }
+
+  _nestedQueryString(params, parent) {
+    var queryParts = [];
+    var param;
+    var value;
+    for (param in params) {
+      value = params[param];
+      param = window.encodeURIComponent(param);
+      if (typeof value === 'object') {
+        queryParts.push(this._nestedQueryString(value, param))
+      } else if (Array.isArray(value)) {
+        for (var i = 0; i < value.length; i++) {
+          if (parent) {
+            queryParts.push(`${parent}[${param}]=${window.encodeURIComponent(value[i])}`);
+          } else {
+            queryParts.push(`${param}=${window.encodeURIComponent(value[i])}`);
+          }
+        }
+      } else if (value !== null) {
+        if (parent) {
+          queryParts.push(`${parent}[${param}]=${window.encodeURIComponent(value)}`);
+        } else {
+          queryParts.push(`${param}=${window.encodeURIComponent(value)}`);
+        }
+      } else {
+        if (parent) {
+          queryParts.push(`${parent}[${param}]`);
+        } else {
+          queryParts.push(`${param}`);
+        }
+      }
+    }
+    return queryParts.join('&');
+  }
+}
+customElements.define(StripeCard.is, StripeCard);
